5f2994316c5a0dbc69ebd075c1361d89eac2989b
yitingfan <helloworldtxt@163.com>
2020-03-09 16:26:40 -0400

    Add files via upload

---
+++ encryption.py
@@ -0,0 +1,47 @@
+#python3
+from random import randint
+import vigenerecipher
+
+C1='gunfights outjuts molters forgot bedclothes cirrus servomotors tumulus incompleteness provoking sixteens breezeways layoff marinas directives teabowl vugs mainframe gazebo bushwhacks testers incompressibility unthoughtfully rivalled repaint nonuple guerre semiaquatic flashgun esthetics icefall touchups baltic baba gorget groper remittances nimbus podium reassurance preventable overroasts chests interchangeable pentarch doctoring potentiated salts overlay rustled recyclability version mottled lee'
+
+C2='intersectional marquees undeniably curates papa invidiousness libidinal congratulate annexion stompers oxblood relicense incept viny dimers typicality meteors indebtedness triceratops statisms arsenides horsed melanin smelt ulsters films townfolk orchestrations disintoxication ceiled allegories pinsetters misdeliveries firebreak baronages sphere stalest amino linkboy plasm avers cocktail reconfirming rearoused paternity moderation pontificated justices overplays borzois trailblazers smelters cor'
+
+C3='frosteds shelters tannest falterer consoles negroes creosote lightful foreshadow mustangs despatches unofficially sanitarium single integrates nebula del stubby impoliteness royal ariel triceratops episcopalians pensive passports largesses manwise repositioned specified promulgates polled fetus immune extinguisher paradise polytheist abdicated ables exotica redecorating embryological scintillatingly shysters parroted twosomes spermicide adapters illustrators suffusion bonze alnicoes acme clair p'
+
+C4='distributee hermitage talmudic thruput apologues recapitulate keyman palinodes semiconscious fauns culver evicts stubbornness stair virginals unto leonardo lyrist merci procuration repulsing medicated lagoons cohort caravans pampas maundered riggings undersell investigator arteriolar unpolled departmentalization penchants shriveled obstreperous misusing synfuels strewn ottawas novelising cautiously foulmouthed travestied bifurcation classicists affectation inverness emits admitter bobsledded erg'
+
+C5='undercurrents laryngeal elevate betokened chronologist ghostwrites ombres dollying airship probates music debouching countermanded rivalling linky wheedled heydey sours nitrates bewares rideable woven rerecorded currie vasectomize mousings rootstocks langley propaganda numismatics fucked subduers babcock jauntily ascots nested notifying mountainside dirk chancellors disassociating eleganter radiant convexity appositeness axonic trainful nestlers applicably correctional stovers organdy bdrm insis'
+
+# key = 12345679012345679010
+# print(type(key))
+# print(candidate_index(key))
+
+l = [C1, C2, C3, C4, C5]
+dict = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26, ' ':27}
+dict2 = {1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 8:'h', 9:'i', 10:'j', 11:'k', 12:'l', 13:'m', 14:'n', 15:'o', 16:'p', 17:'q', 18:'r', 19:'s', 20:'t', 21:'u', 22:'v', 23:'w', 24:'x', 25:'y', 26:'z', 27:' '}
+candidate_index = randint(0, 4)     #random select one candidate_indexing from the five
+L = 500
+t = randint(1, 24)
+origin_key = []
+# Generate random key
+for i in range(t):
+    origin_key.append(randint(0, 26))
+#print(origin_key)
+
+# key scheduling alogrithm
+first_key = []
+for i in range(1, t+1):
+    first_key.append(origin_key[(1 + i) % t])
+
+print(t, '\t', candidate_index, '\t', first_key)
+
+for item in range(L):
+    modified_c = (dict[l[candidate_index][item]] + first_key[item % t]) % 27
+    if modified_c == 0:
+        character = ' '
+    else:
+        character = dict2[modified_c]
+    l[candidate_index] = l[candidate_index][:item] + character + l[candidate_index][item+1:]
+
+
+print(l[candidate_index])   #output ciphertext
