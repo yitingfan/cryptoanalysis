b8b5fcbf589f6735274516df242871c6dd52670e
backcover7 <hou1417x@gmail.com>
2020-03-13 20:45:50 -0400

    Update project.py
    update max var in correct()

--- project.py
+++ project.py
@@ -30,6 +30,7 @@ def correct(plaintext):
                 score = difflib.SequenceMatcher(None, p, w).quick_ratio()
                 if score > max:
                     candidate = p
+                    max = score
             w = candidate
     for w in plain_wordList:
         result = result + w + ' '
@@ -73,8 +74,7 @@ def space(letter, m):
 def keyword(Ciphertext,keylength):
     ListCiphertext = list(Ciphertext)
     Standard = {'a':0.0655307059, 'b':0.0127076566, 'c':0.0226508836, 'd':0.0335227377, 'e':0.1021787708, 'f':0.0197180888, 'g':0.0163586607, 'h':0.0486220925, 'i':0.0573425524, 'j':0.0011440544, 'k':0.0056916712, 'l':0.0335616550, 'm':0.0201727037, 'n':0.0570308374, 'o':0.0620055405, 'p':0.0150311560, 'q':0.0008809302, 'r':0.0497199926, 's':0.0532626738, 't':0.0750999398, 'u':0.0229520040, 'v':0.0078804815, 'w':0.0168961396, 'x':0.0014980832, 'y':0.0146995463, 'z':0.0005979301, ' ':0.1831685753}
-
-    #while True:
+    
     KeyResult = []
     for i in range(keylength):
         PresentCipherList = ListCiphertext[i::keylength]
@@ -91,7 +91,6 @@ def keyword(Ciphertext,keylength):
                 QuCoincidenceMax = QuCoincidencePresent
                 KeyLetter = m
         KeyResult.append(KeyLetter)
-    #    break
     return KeyResult
 
 def decrypt(cipher, keylength, key):
